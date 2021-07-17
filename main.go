d3cb6a5757d99011437cf28ce374e2ba1d8a39a0
woodongho <wdh5518@gmail.com>
2021-07-17 11:05:31 +0900

    4

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ func main() {
 	bc.AddBlock("Send 2 Token to TEMP")
 
 	for _, block := range bc.blocks {
-		fmt.Println("Prev. Hash: %x", block.PrevBlockHash)
-		fmt.Println("Data: %s", block.Data)
-		fmt.Println("Hash: %x", block.Hash)
+		fmt.Printf("Prev. Hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
 		fmt.Println()
 	}
 }
